9f1fa748f7d6a46fd3ba6aafc3083d4c99d1c376
gabs <gabrielahouser1@gmail.com>
2025-04-22 13:49:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from functools import wraps
 
+# this is for authentication
+# if someone tries to access smth they shouldnt, this will block that
 def deco():
     def _deco(f):
         @wraps(f)
@@ -21,10 +23,11 @@ def deco():
 
     return _deco
 
+
 app = Flask(__name__)
 
-@app.route('/hello', methods=["GET", "POST"])
-def hello():
+@app.route('/test', methods=["GET", "POST"])
+def test():
     match request.method:
         case "GET":
             return "Hello, this is GET"
@@ -33,22 +36,61 @@ def hello():
         case _:
             return "WHAT?"
 
-@app.route("/users", methods=["POST"])
+
+@app.route("/users/<userid>", methods=["POST"])
 @deco()
-def users_post():
-    user_id = request.user_id
+def users_post(userid):
+    user_id = userid #request.user_id
     if not user_id:
         return "No user id!", 400
 
     return f"POSTED UserId: {user_id}", 200
 
-@app.route("/users", methods=["GET"])
-def users_get():
+@app.route("/users/<userid>", methods=["GET"])
+def users_get(userid):
     user_id = request.args.get("userId")
     if not user_id:
         return "No user id!", 400
 
-    return f"GET UserId: {user_id}", 200
+    return f"GET UserId: {user_id}", 200 # maybe change to just returning userid
+
+
+@app.route("/users/<userid>/steps", methods=["POST"])
+@deco()
+def steps_post(userid):
+    # frontend needs to do smth with this to get apple health to update
+    user_steps = request.user_steps
+    # get user id of person trying to change and check
+    if not user_steps:
+        return "no user steps", 400
+    
+    return f"POST User steps = {user_steps}"
+
+@app.route("/users/<userid>/steps", methods=["GET"])
+def steps_get():
+    user_steps = request.args.get("userSteps")
+    if not user_steps:
+        return "no user steps", 400
+    
+    return f"GET Usersteps = {user_steps}"
+
+
+
+@app.route("/users/<userid>/achievements", methods=["GET"])
+def achievements_get(userid):
+    user_achievements = request.args.get("userAchievements")
+    if not user_achievements:
+        return "no achievements", 400
+    return f"GET user ach = {user_achievements}"
+
+@app.route("/users/<userid>/achievements", methods=["POST"])
+@deco
+def achievements_post(userid):
+    user_achievements = request.user_achiements
+    if not user_achievements:
+        return "no user achievements", 400
+    
+    return f"POST User achievements = {user_achievements}"
 
 if __name__ == '__main__':
     app.run(debug=True)
